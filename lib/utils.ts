b79eaa6e3b5de8ee71ba2b427c5d703fb2e7c39d
moe <moe@email.com>
2025-08-21 02:33:24 -0500

    zod validation and type inference

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
+export function cn (...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// utility function that is converting the prisma object into a regular JS object 
-export function convertToPlainObject<T>(value: T): T {
+// utility function that is converting the prisma object into a regular JS object
+export function convertToPlainObject<T> (value: T): T {
   return JSON.parse(JSON.stringify(value))
+}
+
+// format number with decimal places
+export function formatNumberWithDecimal (num: number): string {
+  const [int, decimal] = num.toString().split('.')
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`
 }
